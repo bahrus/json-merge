80ce1fc325dca43868faa7acbe8389bfd1e0ee9a
bahrus <anderson.bruce.b@gmail.com>
2018-10-06 16:59:36 -0400

    missing withPath

--- createIIFE.js
+++ createIIFE.js
@@ -1,3 +1,3 @@
 const jiife = require('jiife');
 const xl = 'node_modules/xtal-latx/';
-jiife.processFiles([xl + 'define.js', xl + 'xtal-latx.js', 'xtal-insert-json.js', 'xtal-json-merge.js'], 'json-merge.js');
+jiife.processFiles([xl + 'define.js', xl + 'with-path.js', xl + 'xtal-latx.js', 'xtal-insert-json.js', 'xtal-json-merge.js'], 'json-merge.js');
--- json-merge.js
+++ json-merge.js
@@ -9,6 +9,49 @@
     }
     customElements.define(tagName, custEl);
 }
+const with_path = 'with-path';
+/**
+ * Custom Element mixin that allows a property to be namespaced
+ * @param superClass
+ */
+function WithPath(superClass) {
+    return class extends superClass {
+        /**
+        * @type {string}
+        * object inside a new empty object, with key equal to this value.
+        * E.g. if the incoming object is {foo: 'hello', bar: 'world'}
+        * and with-path = 'myPath'
+        * then the source object which be merged into is:
+        * {myPath: {foo: 'hello', bar: 'world'}}
+        */
+        get withPath() {
+            return this._withPath;
+        }
+        set withPath(val) {
+            this.setAttribute(with_path, val);
+        }
+        wrap(obj) {
+            if (this._withPath) {
+                let mergedObj = {};
+                const retObj = mergedObj;
+                const splitPath = this._withPath.split('.');
+                const lenMinus1 = splitPath.length - 1;
+                splitPath.forEach((pathToken, idx) => {
+                    if (idx === lenMinus1) {
+                        mergedObj[pathToken] = obj;
+                    }
+                    else {
+                        mergedObj = mergedObj[pathToken] = {};
+                    }
+                });
+                return retObj;
+            }
+            else {
+                return obj;
+            }
+        }
+    };
+}
 const disabled = 'disabled';
 /**
  * Base class for many xtal- components
