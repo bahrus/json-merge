b8877aa1fb7b6bf07726d04a94c6e69787139f25
bahrus <anderson.bruce.b@gmail.com>
2019-10-20 09:01:19 -0400

    ...

--- createIIFE.js
+++
@@ -1,3 +0,0 @@
-const jiife = require('jiife');
-const xl = 'node_modules/xtal-latx/';
-jiife.processFiles([xl + 'define.js', xl + 'with-path.js', xl + 'xtal-latx.js', xl + 'mergeDeep.js', 'xtal-insert-json.js', 'xtal-json-merge.js'], 'json-merge.js');
